4f61ff630333f0cc34af590096069e7d70193a84
Matt <mmuhr88@gmail.com>
2017-10-27 14:13:38 -0700

    refigured port

--- server.js
+++ server.js
@@ -1,4 +1,3 @@
-const port = process.env.PORT || 3000;
 const express = require('express')
 const app = express()
 const knex = require('./knex')
@@ -11,6 +10,6 @@ app.get('/data', function (req, res) {
   // })
 })
 
-app.listen(3000, function () {
+app.listen( process.env.PORT||3000, function () {
   console.log('Example app listening on port 3000!')
 })
