447ffc01e7e51f67e8ba9ce5bb6374a6224f4780
Matt <mmuhr88@gmail.com>
2017-10-27 16:24:00 -0700

    added cors

--- server.js
+++ server.js
@@ -2,6 +2,12 @@ const express = require('express')
 const app = express()
 const knex = require('./knex')
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 app.get('/data', function (req, res) {
   return knex('totals').select('*')
   .then((data) => {
