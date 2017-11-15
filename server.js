8f70b7205c81e6f80ceed9bc1b8adaab600816c4
Matt <mmuhr88@gmail.com>
2017-11-14 19:18:12 -0800

    it works!

--- server.js
+++ server.js
@@ -10,31 +10,112 @@ app.use(function(req, res, next) {
 });
 
 app.get('/data', function (req, res) {
-  var results = '"name","number"\n';
+  var results = '"name","nav total","total eur","total usd","total inr"\n';
+  var bondString = '"bond",';
+  var swapsString = '"swaps",';
+  var fxString = '"fx",';
+  var fxoptionsString = '"fx options",';
+  var equitiesString = '"equities",';
+
+  // return knex('data').select('product','nav', 'currency')
+  // .then((data) => {
+  //
+  // })
   return knex('data').sum('nav').where('product', 'bond')
   .then((data) => {
-    results = results + '"bond",' + data[0]['sum'] + '\n';
-    return knex('data').sum('nav').where('product', 'swaps')
-    // return res.status(200).send(results);
+    bondString = bondString + data[0]['sum'] + ',';
+    return knex('data').sum('nav').where({'currency':'eur','product':'bond'})
   })
   .then((data) => {
-    results = results + '"swaps",' + data[0]['sum'] + '\n';
+    bondString = bondString + data[0]['sum'] + ',';
+    return knex('data').sum('nav').where({'currency':'usd','product':'bond'})
+  })
+  .then((data) => {
+    bondString = bondString + data[0]['sum'] + ',';
+    return knex('data').sum('nav').where({'currency':'inr','product':'bond'})
+  })
+  .then((data) => {
+    bondString = bondString + data[0]['sum'] + '\n';
+    return knex('data').sum('nav').where('product', 'swaps')
+  })
+  .then((data) => {
+    swapsString = swapsString + data[0]['sum'] + ',';
+    return knex('data').sum('nav').where({'currency':'eur','product':'swaps'})
+  })
+  .then((data) => {
+    swapsString = swapsString + data[0]['sum'] + ',';
+    return knex('data').sum('nav').where({'currency':'usd','product':'swaps'})
+  })
+  .then((data) => {
+    swapsString = swapsString + data[0]['sum'] + ',';
+    return knex('data').sum('nav').where({'currency':'inr','product':'swaps'})
+  })
+  .then((data) => {
+    swapsString = swapsString + data[0]['sum'] + '\n';
     return knex('data').sum('nav').where('product', 'fx')
   })
   .then((data) => {
-    results = results + '"fx",' + data[0]['sum'] + '\n';
+    fxString = fxString + data[0]['sum'] + ',';
+    return knex('data').sum('nav').where({'currency':'eur','product':'fx'})
+  })
+  .then((data) => {
+    fxString = fxString + data[0]['sum'] + ',';
+    return knex('data').sum('nav').where({'currency':'usd','product':'fx'})
+  })
+  .then((data) => {
+    fxString = fxString + data[0]['sum'] + ',';
+    return knex('data').sum('nav').where({'currency':'inr','product':'fx'})
+  })
+  .then((data) => {
+    fxString = fxString + data[0]['sum'] + '\n';
     return knex('data').sum('nav').where('product', 'fx options')
   })
   .then((data) => {
-    results = results + '"fx options",' + data[0]['sum'] + '\n';
+    fxoptionsString = fxoptionsString + data[0]['sum'] + ',';
+    return knex('data').sum('nav').where({'currency':'eur','product':'fx options'})
+  })
+  .then((data) => {
+    fxoptionsString = fxoptionsString + data[0]['sum'] + ',';
+    return knex('data').sum('nav').where({'currency':'usd','product':'fx options'})
+  })
+  .then((data) => {
+    fxoptionsString = fxoptionsString + data[0]['sum'] + ',';
+    return knex('data').sum('nav').where({'currency':'inr','product':'fx options'})
+  })
+  .then((data) => {
+    fxoptionsString = fxoptionsString + data[0]['sum'] + '\n';
     return knex('data').sum('nav').where('product', 'equities')
   })
   .then((data) => {
-    results = results + '"equities",' + data[0]['sum'] + '\n';
-    return res.status(200).send(results);
+    equitiesString = equitiesString + data[0]['sum'] + ',';
+    return knex('data').sum('nav').where({'currency':'eur','product':'equities'})
   })
-})
+  .then((data) => {
+    equitiesString = equitiesString + data[0]['sum'] + ',';
+    return knex('data').sum('nav').where({'currency':'usd','product':'equities'})
+  })
+  .then((data) => {
+    equitiesString = equitiesString + data[0]['sum'] + ',';
+    return knex('data').sum('nav').where({'currency':'inr','product':'equities'})
+  })
+  .then((data) => {
+    equitiesString = equitiesString + data[0]['sum'] + '\n';
+    results = results + bondString + swapsString + fxString + fxoptionsString + equitiesString;
+    return res.status(200).send(results);
+  });
+  // .then((data) => {
+  //   results = results + '"fx",' + data[0]['sum'] + '\n';
+  //   return knex('data').sum('nav').where('product', 'fx options')
+  // })
+  // .then((data) => {
+  //   results = results + '"fx options",' + data[0]['sum'] + '\n';
+  //   return knex('data').sum('nav').where('product', 'equities')
+  // })
+  // .then((data) => {
+  //   results = results + '"equities",' + data[0]['sum'] + '\n';
+  //   return res.status(200).send(results);
+});
 
 app.listen(PORT, () => {
   console.log('Example app listening on port 3000!');
-})
+});
